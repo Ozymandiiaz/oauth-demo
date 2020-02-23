e9381720bd5cd38d05133628cdac196e2b6df499
Chris Hundhausen <chundhau@gmail.com>
2020-02-23 13:41:13 -0800

    new build for client

---
+++ client/build/precache-manifest.c9efb34fd176570e5a5ffb7afcd36945.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b0e128662122ac9b2c90",
+    "url": "/static/css/main.ea98eae3.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "b7a103295f6945a79b57",
+    "url": "/static/js/1.b7a10329.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b0e128662122ac9b2c90",
+    "url": "/static/js/main.b0e12866.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "b7a103295f6945a79b57",
+    "url": "/static/css/1.895e2dd3.chunk.css"
+  },
+  {
+    "revision": "36007c7957c852c2a08bdf8c7b56a850",
+    "url": "/index.html"
+  }
+];
