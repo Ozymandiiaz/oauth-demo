549f43f44a52338c92b063774242116b7bcfdcd9
Chris Hundhausen <chundhau@gmail.com>
2020-02-23 13:39:01 -0800

    Further tweaks

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap-social/assets/css/bootstrap.css';
+//import 'bootstrap-social/assets/css/bootstrap.css';
+//import 'bootstrap-social/assets/css/font-awesome.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
 import './index.css';
